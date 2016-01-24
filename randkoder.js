7cc0cde68e1c09bc08e65a88b746ed8e1383049d
Tomasz Kuter <evolic@tomaszkuter.com>
2016-01-24 12:18:30 +0100

    [programs] Added fifth pair of the programs introducing headers exchange:  * randkoder.js  * swatka.js

---
+++ randkoder.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+var args = process.argv.slice(2);
+var numberOfAttributes = 3;
+
+if (args.length !== numberOfAttributes) {
+  console.log("Korzystanie: randkoder.js [imię] [płeć: k|m] [kolor włosów: blond|rude|siwe|czarne]");
+  process.exit(1);
+}
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'randkomierz';
+    var args = process.argv.slice(2);
+    var name = args[0];
+    var attributes = args.slice(1);
+    var headersList = ['plec', 'kolor-wlosow'];
+    var headersValues = {};
+
+    for (var i=0; i<numberOfAttributes; i++) {
+      var headerName  = headersList[i];
+      var headerValue = attributes[i];
+
+      headersValues[headerName] = headerValue;
+    }
+
+    ch.assertExchange(ex, 'headers', {durable: false});
+    ch.publish(ex, '', new Buffer(name), {headers: headersValues});
+
+    console.log(" [x] Wysłano profil '%s'", name);
+  });
+
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
